07d1f6e5692bfb2f7573b4a6994eb27d59bd9894
bushinnn <adebayobushrah321@gmail.com>
2022-06-16 12:58:53 +0100

    my first message

---
+++ script.js
@@ -0,0 +1,18 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const first = form['firstname'].value;
+    const last = form['lastname'].value;
+    const email = form['email'].value;
+    const password = form['password'].value;
+
+    if(firstName === '') {
+        const small = form['firstname'].parentNode.querySelector('small')
+        small.innerText = 'first name is required';
+        small.style.opacity = '1'
+    }
+
+    alert(first + last + email + password);
+});
